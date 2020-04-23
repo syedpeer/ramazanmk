dcb0ba23d80fef4854eb9754baf353341b4e140b
Lokman Musliu <lokman.musliu@gmail.com>
2020-04-23 18:09:06 +0200

    Updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,123 @@
 <template>
+  <div id="app" class="font-sans antialiased flex flex-col">
+    <div class="container mx-auto pt-32">
+      <div class="max-w-md">
+        <timer :start="this.current" class="mb-4 font-bold text-xl"></timer>
+        <select
+          v-if="location.length"
+          class="w-64 h-12 bg-gray-300"
+          v-model="city"
+          name="city"
+          id="city"
+        >
+          <option
+            v-for="(item, index) in location"
+            :key="index"
+            :value="item.shortcode"
+          >{{ item.name }}</option>
+        </select>
+        <div v-if="schedule.length" class="flex flex-row mt-12 justify-between">
+          <div class="flex flex-col">
+            <p v-for="item in getIftarTime" :key="item.id">{{ item }}</p>
+          </div>
 
+          <div class="flex flex-col">
+            <p v-for="item in getSyfirTime" :key="item.id">{{ item }}</p>
+          </div>
+        </div>
+
+        <div class="mt-32 font-bold text-xl">{{ getTodayIftar }}</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import sheetReader from "./modules/sheetReader";
+import json from "./data/schedule.json";
+import Timer from "./components/Timer";
 
 export default {
   name: "app",
   components: {
+    Timer
   },
   data() {
     return {
-      schedule: [],
-      location: []
+      city: "GV",
+      current: {
+        date: this.$dayjs(new Date()).format("YYYY-MM-DD HH:mm:ss"),
+        city: null,
+        schedule: null
+      },
+      schedule: json.schedule,
+      location: json.city
     };
   },
-  mounted() {
-    const schedule = {
-      docId: 1,
-      sheetId: "1dz8EUh67OOunhEcVqLjTg3nvjCYLmf6_JBFXvs4vCbs",
-      returnAllResults: true
-    };
+  methods: {
+    getCurrentData() {
+      this.current.city = this.location.find(item => {
+        return item.shortcode === this.city;
+      });
 
-    const location = {
-      docId: 2,
-      sheetId: "1dz8EUh67OOunhEcVqLjTg3nvjCYLmf6_JBFXvs4vCbs",
-      returnAllResults: true
-    };
+      this.current.schedule = this.getToday();
+    },
+    formatDate(item) {
+      return this.$dayjs(item)
+        .add(this.current.city.value, "minute")
+        .format("HH:mm");
+    },
+    getToday(day = 1) {
+      let date = this.$dayjs(this.current.date)
+        .add(day, "day")
+        .format("YYYY-MM-DD");
 
-    sheetReader(schedule, results => {
-      this.schedule.push(results);
-    });
+      return this.schedule.find(item => {
+        return item.data === date.toString();
+      });
+    }
+  },
+  computed: {
+    getIftarTime() {
+      return this.schedule.map(item => {
+        return this.formatDate(item.iftar);
+      });
+    },
 
-    sheetReader(location, results => {
-      this.location.push(results);
-    });
+    getSyfirTime() {
+      return this.schedule.map(item => {
+        return this.formatDate(item.sifir);
+      });
+    },
+
+    getTodayIftar() {
+      // TODO: Switch to this.current.date
+      const today = new Date("2020-04-24 20:36");
+      const difference = this.$dayjs(this.current.schedule.iftar).diff(
+        today,
+        "hour",
+        true
+      );
+
+      if (difference > 0) {
+        return this.$dayjs(this.current.schedule.iftar);
+      }
+
+      if (difference < 0) {
+        const date = this.getToday(2);
+
+        return this.formatDate(date.iftar);
+      }
+    }
+  },
+  watch: {
+    city() {
+      this.getCurrentData();
+    }
+  },
+  created() {
+    this.getCurrentData();
+
+    this.$dayjs.locale("sq");
   }
 };
 </script>
