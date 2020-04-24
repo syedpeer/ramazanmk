573b8a7725d8aa329d7cec9eb7d62b223a4ced1b
Arlind Musliu <arlindmusliu90@gmail.com>
2020-04-24 11:25:19 +0200

    fixed turkish language issues

--- src/components/BaseTable.vue
+++ src/components/BaseTable.vue
@@ -3,7 +3,7 @@
     <div v-for="(item, index) in this.items" class="flex flex-row pt-4 pb-2 border-b border-gray-300">
       <div class="w-1/2 flex flex-col">
         <p class="text-xl font-semibold">{{ dayNumber(item.data) }}</p>
-        <p class="text-sm uppercase">{{ dayText(item.data) }}</p>
+        <p class="text-sm capitalize">{{ dayText(item.data) }}</p>
       </div>
       <div class="w-1/4 flex flex-col items-end pr-4">
         <p class="text-sm uppercase">{{ $t('syfyr') }}</p>
