6e4fae4157f9a46dba7cb3a0f3adbcd9e31f8400
Lokman Musliu <lokman.musliu@gmail.com>
2020-04-28 15:28:42 +0200

    Fixed padding issue

--- src/components/BaseTable.vue
+++ src/components/BaseTable.vue
@@ -1,17 +1,14 @@
 <template>
   <div
-    class="w-full text-primary lg:overflow-y-auto bg-white shadow-input py-6 rounded-tl-2xl lg:rounded-tl-2xl lg:rounded-bl-2xl lg:max-h-50"
+    class="w-full text-primary lg:overflow-y-auto bg-white shadow-input rounded-tl-2xl lg:rounded-tl-2xl lg:rounded-bl-2xl lg:max-h-50"
   >
     <div
       v-for="(item, index) in this.items"
-      class="flex flex-row py-4 px-6 border-b border-grey-50 relative"
+      class="flex flex-row py-4 px-6 border-grey-50 relative"
       :class="{'border-b' : index + 1 !== items.length}"
       :key="index"
     >
-      <div
-        v-if="today === item.data"
-        class="absolute left-0 top-0 w-2 h-full bg-accent"
-      ></div>
+      <div v-if="today === item.data" class="absolute left-0 top-0 w-2 h-full bg-accent"></div>
       <div class="w-1/2 flex flex-col">
         <p class="text-xl font-semibold">{{ dayNumber(item.data) }}</p>
         <p class="text-sm capitalize">{{ dayText(item.data) }}</p>
